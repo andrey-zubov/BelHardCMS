70f69c7292b3c414e77e3b9df11c1996544fa190
Sanello <sanello@gmail.com>
2019-10-07 16:27:17 +0300

    25-th commit. I use ID instead slug

--- client/client_urls.py
+++ client/client_urls.py
@@ -61,14 +61,18 @@ urlpatterns = [
     path(r'settingsset/', views.set_settings, name='settings_set'),
     path(r'chat_update/', views.chat_update, name='chat_update'),
 
-    # Poland urls
-    path('cvs/', resumes_list, name='resumes_list_url'),
-    path('cv_detail/<id_c>/', resume_detail, name='resume_detail_url'),
-    path('cv/<id_c>/accepted_vacancies/', accepted_vacancies, name='accepted_vacancies_url'),
-    path('cv/<id_c>/rejected_vacancies/', rejected_vacancies, name='rejected_vacancies_url'),
-    path('cv/vacancy/<id_v>/', vacancy_detail, name='vacancy_detail_url'),
+    
+    ##Poland urls
+    path('resumes/', resumes_list, name='resumes_list_url'),
+    path('resumes/<id>/', resume_detail, name='resume_detail_url'),
+    path('resumes/<id>/accepted_vacancies/', accepted_vacancies, name='accepted_vacancies_url'),
+    path('resumes/<id>/rejected_vacancies/', rejected_vacancies, name='rejected_vacancies_url'),
+    path('resumes/<id>/vacancies/', vacancies_list, name='vacancies_list_url'),
+    path('resumes/vacancy/<id>/', vacancy_detail, name='vacancy_detail_url'),
     path('accept_reject/', views.accept_reject),
     path('help/', help_list, name='help_list_url'),
+    path('settings/', settings_list, name='settings_list_url'),
+    path('on_off/', views.on_off),    # on_off settings for notifications
     path('viewed/', views.viewed),
 
 ]
