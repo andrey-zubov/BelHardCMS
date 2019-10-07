dbdd744cb61f55e27b8a002e8289178deabfaad7
Sanello <sanello@gmail.com>
2019-10-07 23:40:28 +0300

    26-th commit. The addition of CVs isn't worked.

--- client/client_urls.py
+++ client/client_urls.py
@@ -55,14 +55,12 @@ urlpatterns = [
     path(r'settings/', views.settings_menu, name='settings_menu'),
     path(r'settingsset/', views.set_settings, name='settings_set'),
 
-    
-    ##Poland urls
-    path('resumes/', resumes_list, name='resumes_list_url'),
-    path('resumes/<id>/', resume_detail, name='resume_detail_url'),
-    path('resumes/<id>/accepted_vacancies/', accepted_vacancies, name='accepted_vacancies_url'),
-    path('resumes/<id>/rejected_vacancies/', rejected_vacancies, name='rejected_vacancies_url'),
-    path('resumes/<id>/vacancies/', vacancies_list, name='vacancies_list_url'),
-    path('resumes/vacancy/<id>/', vacancy_detail, name='vacancy_detail_url'),
+    # Poland urls
+    path('cv/', resumes_list, name='resumes_list_url'),
+    path('cv/<id_c>/', resume_detail, name='resume_detail_url'),
+    path('cv/<id_c>/accepted_vacancies/', accepted_vacancies, name='accepted_vacancies_url'),
+    path('cv/<id_c>/rejected_vacancies/', rejected_vacancies, name='rejected_vacancies_url'),
+    path('cv/vacancy/<id_v>/', vacancy_detail, name='vacancy_detail_url'),
     path('accept_reject/', views.accept_reject),
     path('help/', help_list, name='help_list_url'),
     path('settings/', settings_list, name='settings_list_url'),
