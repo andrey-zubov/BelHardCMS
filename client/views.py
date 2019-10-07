6d2c5e052253b5b192604b85dc4d0a0db3534741
Sanello <sanello@gmail.com>
2019-10-07 16:27:17 +0300

    25-th commit. I use ID instead slug

--- client/views.py
+++ client/views.py
@@ -621,15 +621,15 @@ def set_settings(request):
     return HttpResponse(settings)
 
 
-#Poland's views
+# Poland's views ###################################################################################
 
 def vacancies_list(request, slug):
-    resume = Resume.objects.get(slug__iexact=slug)
+    resume = Resume.objects.get(id=id)
     return render(request, 'client/client_vacancies.html', context={'resume': resume})
 
 
-def vacancy_detail(request, slug):
-    vacancy = Vacancy.objects.get(slug__iexact=slug)
+def vacancy_detail(request, id):
+    vacancy = Vacancy.objects.get(id=id)
     first_flag = 1 if bool(vacancy.in_waiting_for_resume.all() or vacancy.reject_for_resume.all()) else 0
     second_flag = 1 if bool(vacancy.in_waiting_for_resume.all() or vacancy.accept_for_resume.all()) else 0
     return render(request, 'client/client_vacancy_detail.html', context={
@@ -644,54 +644,54 @@ def resumes_list(request):
     return render(request, 'client/client_resumes.html', context={'resumes': resumes})
 
 
-def resume_detail(request, slug):
-    resume = Resume.objects.get(slug__iexact=slug)
+def resume_detail(request, id):
+    resume = Resume.objects.get(id=id)
     return render(request, 'client/client_resume_detail.html', context={'resume': resume})
 
 
-def accepted_vacancies(request, slug):########################
-    resume = Resume.objects.get(slug__iexact=slug)
+def accepted_vacancies(request, id):
+    resume = Resume.objects.get(id=id)
     return render(request, 'client/client_accepted_vacancies.html', context={'resume': resume})
 
 
-def rejected_vacancies(request, slug):##############################
-    resume = Resume.objects.get(slug__iexact=slug)
+def rejected_vacancies(request, id):
+    resume = Resume.objects.get(id=id)
     return render(request, 'client/client_rejected_vacancies.html', context={'resume': resume})
 
 
-def accept_reject(request):#
+def accept_reject(request):
 
-    if request.GET['flag'] == 'accept' and Vacancy.objects.get(slug__iexact=request.GET['slug']).in_waiting_for_resume.all():
-        print(request.GET['slug'], 1)
-        r = Vacancy.objects.get(slug__iexact=request.GET['slug']).in_waiting_for_resume.get()
-        v = Vacancy.objects.get(slug__iexact=request.GET['slug'])
+    if request.GET['flag'] == 'accept' and Vacancy.objects.get(id=request.GET['id_v']).in_waiting_for_resume.all():
+        print(request.GET['id'], 1)
+        r = Vacancy.objects.get(id=request.GET['id_v']).in_waiting_for_resume.get()
+        v = Vacancy.objects.get(id=request.GET['id_v'])
         r.vacancies_accept.add(v)
         r.vacancies_in_waiting.remove(v)
         r.save()
         return HttpResponse('accept_server')
 
-    elif request.GET['flag'] == 'reject' and Vacancy.objects.get(slug__iexact=request.GET['slug']).in_waiting_for_resume.all():
-        print(request.GET['slug'], 2)
-        r = Vacancy.objects.get(slug__iexact=request.GET['slug']).in_waiting_for_resume.get()
-        v = Vacancy.objects.get(slug__iexact=request.GET['slug'])
+    elif request.GET['flag'] == 'reject' and Vacancy.objects.get(id=request.GET['id_v']).in_waiting_for_resume.all():
+        print(request.GET['id_v'], 2)
+        r = Vacancy.objects.get(id=request.GET['id_v']).in_waiting_for_resume.get()
+        v = Vacancy.objects.get(id=request.GET['id_v'])
         r.vacancies_reject.add(v)
         r.vacancies_in_waiting.remove(v)
         r.save()
         return HttpResponse('reject_server')
 
-    elif request.GET['flag'] == 'accept' and Vacancy.objects.get(slug__iexact=request.GET['slug']).reject_for_resume.all():
-        print(request.GET['slug'], 3)
-        r = Vacancy.objects.get(slug__iexact=request.GET['slug']).reject_for_resume.get()
-        v = Vacancy.objects.get(slug__iexact=request.GET['slug'])
+    elif request.GET['flag'] == 'accept' and Vacancy.objects.get(id=request.GET['id_v']).reject_for_resume.all():
+        print(request.GET['id_v'], 3)
+        r = Vacancy.objects.get(id=request.GET['id_v']).reject_for_resume.get()
+        v = Vacancy.objects.get(id=request.GET['id_v'])
         r.vacancies_accept.add(v)
         r.vacancies_reject.remove(v)
         r.save()
         return HttpResponse('accept_server')
 
-    elif request.GET['flag'] == 'reject' and Vacancy.objects.get(slug__iexact=request.GET['slug']).accept_for_resume.all():
-        print(request.GET['slug'], 4)
-        r = Vacancy.objects.get(slug__iexact=request.GET['slug']).accept_for_resume.get()
-        v = Vacancy.objects.get(slug__iexact=request.GET['slug'])
+    elif request.GET['flag'] == 'reject' and Vacancy.objects.get(id=request.GET['id_v']).accept_for_resume.all():
+        print(request.GET['id_v'], 4)
+        r = Vacancy.objects.get(id=request.GET['id_v']).accept_for_resume.get()
+        v = Vacancy.objects.get(id=request.GET['id_v'])
         r.vacancies_reject.add(v)
         r.vacancies_accept.remove(v)
         r.save()
