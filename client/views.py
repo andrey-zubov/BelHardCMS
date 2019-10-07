853c27b1b9f7154097b923a28d35c9e2fc57ffd5
Sanello <sanello@gmail.com>
2019-10-07 23:40:28 +0300

    26-th commit. The addition of CVs isn't worked.

--- client/views.py
+++ client/views.py
@@ -10,6 +10,12 @@ from django.views import View
 from django.http import HttpResponse, JsonResponse
 from django.views.generic import TemplateView
 
+from .models import Vacancy
+
+from .forms import UploadImgForm, AddSkillForm, AddSkillFormSet, OpinionForm, AnswerForm, MessageForm
+
+from django.views.generic import View, TemplateView
+
 from client.edit.check_clients import (client_check, load_client_img)
 from client.edit.edit_forms import (EducationFormSet, CertificateForm, SabClassFormSet, UploadImgForm)
 from client.edit.pages_get import (edit_page_get, skills_page_get, cv_page_get, education_page_get, experience_page_get)
@@ -42,15 +48,15 @@ def client_main_page(request):  # !!!!!!!!!!!!!!!!!!!!!Alert
     context = {'unread_messages': unread_messages, 'readtask': readtask, 'settings': settings}
 
     # Poland
-    resumes = Resume.objects.all()
-    suggestions = 0
-    for resume in resumes:
-        suggestions += resume.notification.count()
-    response['unread_suggestions'] = suggestions
-    client_instance = client_check(request.user)
-    response['client_img'] = load_client_img(client_instance)
-    context.update(response)
-    print(context['unread_suggestions'])
+    # resumes = CV.objects.all()
+    # suggestions = 0
+    # for resume in resumes:
+    #   suggestions += resume.notification.count()
+    # response['unread_suggestions'] = suggestions
+    # client_instance = client_check(request.user)
+    # response['client_img'] = load_client_img(client_instance)
+    # context.update(response)
+    # print(context['unread_suggestions'])
     return render(request=request, template_name='client/main_template_client.html', context=context)
 
 
@@ -340,7 +346,7 @@ def checknotifications(request):
     chat = Chat.objects.get(members=request.user)
     unread_messages = len(Message.objects.filter(chat=chat, is_read=False).exclude(author=request.user))
     readtask = len(Tasks.objects.filter(user=request.user, readtask=False))
-    resumes = Resume.objects.all()
+    resumes = CV.objects.all()
     suggestions = 0
     for resume in resumes:
         suggestions += resume.notification.count()
@@ -408,13 +414,9 @@ def chat_update(request):
 
 # Poland's views
 
-def vacancies_list(request, slug):
-    resume = Resume.objects.get(id=id)
-    return render(request, 'client/client_vacancies.html', context={'resume': resume})
 
-
-def vacancy_detail(request, id):
-    vacancy = Vacancy.objects.get(id=id)
+def vacancy_detail(request, id_v):
+    vacancy = Vacancy.objects.get(id=id_v)
     first_flag = 1 if bool(vacancy.in_waiting_for_resume.all() or vacancy.reject_for_resume.all()) else 0
     second_flag = 1 if bool(vacancy.in_waiting_for_resume.all() or vacancy.accept_for_resume.all()) else 0
     return render(request, 'client/client_vacancy_detail.html', context={
@@ -425,31 +427,29 @@ def vacancy_detail(request, id):
 
 
 def resumes_list(request):
-    resumes = Resume.objects.all()
+    resumes = CV.objects.all()
     client_instance = client_check(request.user)
     return render(request, 'client/client_resumes.html', context={'resumes': resumes,
-                                                                  'client_img':load_client_img(client_instance)})
-
-
-def resume_detail(request, id):
-    resume = Resume.objects.get(id=id)
+                                                                  'client_img': load_client_img(client_instance)})
+def resume_detail(request, id_c):
+    resume = CV.objects.get(id=id_c)
     return render(request, 'client/client_resume_detail.html', context={'resume': resume})
 
 
-def accepted_vacancies(request, id):
-    resume = Resume.objects.get(id=id)
+def accepted_vacancies(request, id_c):
+    resume = CV.objects.get(id=id_c)
     return render(request, 'client/client_accepted_vacancies.html', context={'resume': resume})
 
 
-def rejected_vacancies(request, id):
-    resume = Resume.objects.get(id=id)
+def rejected_vacancies(request, id_c):
+    resume = CV.objects.get(id=id_c)
     return render(request, 'client/client_rejected_vacancies.html', context={'resume': resume})
 
 
 def accept_reject(request):
 
     if request.GET['flag'] == 'accept' and Vacancy.objects.get(id=request.GET['id_v']).in_waiting_for_resume.all():
-        print(request.GET['id'], 1)
+        print(request.GET['id_v'], 1)
         r = Vacancy.objects.get(id=request.GET['id_v']).in_waiting_for_resume.get()
         v = Vacancy.objects.get(id=request.GET['id_v'])
         r.vacancies_accept.add(v)
