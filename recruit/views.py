31fd29167a5d9666caabbf435db8d13c6078b81a
Sanello <sanello@gmail.com>
2019-10-10 16:02:27 +0300

    28-th commit. Recruiter's main page was created.

--- recruit/views.py
+++ recruit/views.py
@@ -2,9 +2,26 @@ from time import perf_counter
 
 from django.shortcuts import redirect, render, get_object_or_404
 from client.models import Client
+from time import perf_counter
+
+from django.shortcuts import redirect, render, get_object_or_404
+from client.models import Client
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
+from django.template.context_processors import csrf
+from django.urls import reverse
+from django.http import HttpResponse
+from .models import *
+
+
+def recruiter_main_page(request):
+    return render(request=request, template_name='recruit/main_template_recruiter.html', )
+
+
+def base_of_clients(request):
+    clients = Client.objects.all()
+    return render(request=request, template_name='recruit/recruiter_base_of_clients.html', context={'clients': clients})
 from django.template.context_processors import csrf
 from .forms import FileFieldForm
 from django.views.generic.edit import FormView
