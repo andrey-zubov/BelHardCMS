554655214d3f3e077a2a611786397669087df6b7
Sanello <sanello@gmail.com>
2019-10-16 09:17:59 +0300

    35th commit. In process

--- client/client_urls.py
+++ client/client_urls.py
@@ -2,17 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from client import views
-from client.edit.load_data_list import (SkillsDataList, InstitutionDataList, CvPositionDataList)
-from client.views import *
-from . import views
-from .views import *
-from . import views
-from .views import *
-from . import views
 from .edit.load_data_list import (SkillsDataList, InstitutionDataList, CvPositionDataList)
 from .views import *
 
-# from BelHardCMS.client.views import ResumesList, ResumeDetail
 
 urlpatterns = [
     path('', views.client_main_page, name='client'),  # main client page
@@ -30,13 +22,6 @@ urlpatterns = [
     path('edit/institution_data_list/', InstitutionDataList.as_view(), name='institution_data_list'),
     path('edit/cv_position_data_list/', CvPositionDataList.as_view(), name='cv_position_data_list'),
     # Team Rome (end)
-    path('edit', views.client_edit_main, name='client_edit'),
-    path('upload', views.upload, name='client_cv_upload'),
-    path('edit/skills', views.client_edit_skills, name='client_edit_skills'),
-    path('edit/photo', views.client_edit_photo, name='client_edit_photo'),
-    path('edit/cv', views.client_edit_cv, name='client_edit_cv'),
-    path('edit/education', views.client_edit_education, name='client_edit_education'),
-    path('edit/experience', views.client_edit_experience, name='client_edit_experience'),
 
     path('chat/', login_required(views.MessagesView.as_view()), name='contact_with_centre'),
     path(r'opinion/', opinion_list, name='opinion_list'),
@@ -44,18 +29,16 @@ urlpatterns = [
     path(r'opinion/<int:pk>/', opinion_detail, name='opinion_detail'),
     path(r'opinion/edit/<int:pk>/', answer_create, name='opinion_answer'),
     path(r'opinion/edit/<int:pk>/delete/', OpinionDelete.as_view(), name='opinion_delete'),
-    path('login/', client_login, name='login'),
-    path('logout/', client_logout, name='logout'),
+    path('login/', client_login),
+    path('logout/', client_logout),
     path('tasks/', tasks, name='tasks_list'),
-
-    # path('edit/form_edu/', views.form_education, name='form_edu'),
 
     path(r'checktask/', views.checktask),
     path(r'checknotifications/', views.checknotifications),
     path(r'settings/', views.settings_menu, name='settings_menu'),
     path(r'settingsset/', views.set_settings, name='settings_set'),
 
-    # Poland urls
+    # Poland urls (start)
     path('cvs/', ResumesList.as_view(), name='resumes_list_url'),
     path('cv_detail/<id_c>/', ResumeDetail.as_view(), name='resume_detail_url'),
     path('cv/<id_c>/accepted_vacancies/', AcceptedVacancies.as_view(), name='accepted_vacancies_url'),
@@ -70,6 +53,7 @@ urlpatterns = [
     path('upload', views.upload, name='client_cv_upload'),
     path('upload', views.upload, name='upload'),
 
+    # # Poland urls (end)
 
 
 
