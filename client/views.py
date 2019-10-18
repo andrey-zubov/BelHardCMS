b3b43c6c06b51f84d3c65662db0ec9e90a1adc5a
Shymaxer_ya <shymaxer.ya@gmail.com>
2019-10-18 17:19:45 +0300

    done email's about new messages

--- client/views.py
+++ client/views.py
@@ -375,7 +375,6 @@ def set_settings(request):
         settings.email_suggestions = status
     elif setting == 'email_meetings':
         settings.email_meetings = status
-
     elif setting == 'email_reviews':
         settings.email_reviews = status
 
--- recruit/views.py
+++ recruit/views.py
@@ -1,7 +1,9 @@
+from django.core.mail import EmailMessage
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
-from client.models import Chat, Message
+from client.models import Chat, Message, Settings
+
 
 def recruit_main_page(request):
     return render(request, template_name='recruit/recruit_main_template.html')
@@ -36,7 +38,18 @@ def get_messages(request):
 def send_message(request):
     chat = Chat.objects.get(id=request.GET['chat_id'])
     mes = Message(chat=chat, author=request.user, message=request.GET['message'])
+    members = chat.members.all()
     mes.save()
+
+    for m in members:
+        if m != request.user:
+            try:
+                if Settings.objects.get(user=m).email_messages:
+                    send_email = EmailMessage('HR-system', 'У вас новое сообщение', to=[str(m.email)])
+                    send_email.send()
+            except Exception:
+                print('Exception: нет адреса электронной почты')
+
     send = {'author_id': mes.author.id, 'author_name': mes.author.username, 'message': mes.message, 'message_id': mes.id,
              'pub_date': mes.pub_date.ctime()}
     return JsonResponse(send, safe=False)
