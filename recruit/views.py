00b51f0f7dd41e9b0e1f81b1dd3d4951ed385e0e
Sanello <sanello@gmail.com>
2019-10-19 00:51:44 +0300

    41th commit. In processing

--- recruit/views.py
+++ recruit/views.py
@@ -48,18 +48,20 @@ class CreateJobInterview(View):
         return render(request, 'recruit/recruiter_tasks_for_applicant.html',
                       context={'applicant_user': applicant_user, 'form': form})
 
-    def post(self, request):
+    def post(self, request, id_a):
+        applicant_user = Client.objects.get(id=id_a)
         bound_form = JobInterviewsForm(request.POST)
-        print(bound_form)
         if bound_form.is_valid():
             print(bound_form)
-            # new_jobinterview = bound_form.save()
-
+            new_jobinterview = bound_form.save()
+            return redirect(applicant_user.get_tasks_url())
+        return render(request, 'recruit/recruiter_tasks_for_applicant.html',
+                      context={'applicant_user': applicant_user, 'form': bound_form})
 
 class Test(View):
     def get(self, request):
         form = TestForm()
-        print(request.POST)
+
         return render(request, 'recruit/test.html', context={'form': form})
 
     def post(self, request):
