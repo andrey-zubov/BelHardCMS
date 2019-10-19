8d2d3e548d7e7e03c405b0a4cdf0b7bab4dd2096
Uberlegsam <konstantin.kizeew@gmail.com>
2019-10-19 15:51:00 +0300

    add favourites to recruit

--- recruit/models.py
+++ recruit/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from client.models import Client
 from django.contrib.auth import get_user_model
 
 RecruiterModel = get_user_model()
@@ -7,6 +6,7 @@ RecruiterModel = get_user_model()
 
 class Recruiter(models.Model):
     recruiter = models.OneToOneField(RecruiterModel, on_delete=models.CASCADE)
-    ownclient = models.ForeignKey(Client,on_delete=models.SET_NULL, null=True, blank=True)
+
+
 
 
