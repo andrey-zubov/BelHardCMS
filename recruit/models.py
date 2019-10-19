e8f42250542901a71952cf22d51e6e2dfbcef419
Uberlegsam <konstantin.kizeew@gmail.com>
2019-10-19 14:19:57 +0300

    add mod rec

--- recruit/models.py
+++ recruit/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from client.models import Client
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+RecruiterModel = get_user_model()
+
+
+class Recruiter(models.Model):
+    recruiter = models.OneToOneField(RecruiterModel, on_delete=models.CASCADE)
+    ownclient = models.ForeignKey(Client,on_delete=models.SET_NULL, null=True, blank=True)
+
 
