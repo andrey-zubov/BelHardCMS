dc5f8f31e13de2c196a28b0cb9f3c75ddb529621
bequite <valentin8dev@gmail.com>
2019-10-20 22:35:19 +0300

    added: color for debug print

--- recruit/edit_pages/check_recruit.py
+++ recruit/edit_pages/check_recruit.py
@@ -1,20 +1,21 @@
-import logging
-
-from client.edit.utility import try_except
+from client.edit.log_color import (log_info, log_error)
+from client.edit.utility import try_except, time_it
 from recruit.models import (Recruit)
 
 
+# TeamRome
 @try_except
+@time_it
 def recruit_check(user):
     """ список карточек c id recruit. """
     users_id_list = [i['user_recruit_id'] for i in Recruit.objects.all().values()]
-    print("recruit_id_list: %s" % users_id_list)
+    log_info("\trecruit_id_list: %s" % users_id_list)
     """ Current Recruit """
-    print("recruit_name: %s, user_recruit_id: %s" % (user, user.id))
+    log_info("\trecruit_name: %s, user_recruit_id: %s" % (user, user.id))
     if user.id in users_id_list:
         recruit = Recruit.objects.get(user_recruit=user)
-        print("recruit_id: %s" % recruit.id)
+        log_info("\trecruit_id: %s" % recruit.id)
         return recruit
     else:
-        logging.warning('User Profile DO NOT exists')
+        log_error('\tUser Profile DO NOT exists')
         return None
