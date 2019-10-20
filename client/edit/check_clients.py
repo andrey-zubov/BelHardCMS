4aaaafa2c0d2e3e7deb3056cee9311b15a11daef
bequite <valentin8dev@gmail.com>
2019-10-20 22:35:19 +0300

    added: color for debug print

--- client/edit/check_clients.py
+++ client/edit/check_clients.py
@@ -1,26 +1,28 @@
-import logging
-
 from BelHardCRM.settings import MEDIA_URL
-from client.edit.utility import try_except
+from client.edit.log_color import (log_info, log_error)
+from client.edit.utility import try_except, time_it
 from client.models import Client
 
 
+# TeamRome
 @try_except
+@time_it
 def client_check(user):
     """ список карточек c id клиента. """
     users_id_list = [i['user_client_id'] for i in Client.objects.all().values()]
-    print("client_id_list: %s" % users_id_list)
+    log_info("\tclient_id_list: %s" % users_id_list)
     """ Current User """
-    print("user_name: %s, user_client_id: %s" % (user, user.id))
+    log_info("\tuser_name: %s, user_client_id: %s" % (user, user.id))
     if user.id in users_id_list:
         client = Client.objects.get(user_client=user)
-        print("user_id: %s" % client.id)
+        log_info("\tuser_id: %s" % client.id)
         return client
     else:
-        logging.warning('User Profile DO NOT exists')
+        log_error('\tUser Profile DO NOT exists')
         return None
 
 
+# TeamRome
 @try_except
 def load_client_img(client):
     """ Show Client Img in the Navigation Bar.
--- client/edit/utility.py
+++ client/edit/utility.py
@@ -1,7 +1,7 @@
-import logging
 import re
-
 from time import perf_counter
+
+from client.edit.log_color import (log_info, log_verbose, log_error)
 
 
 # TeamRome
@@ -9,13 +9,13 @@ def check_input_str(string: str, title=True) -> str or None:
     """ pattern: одно-два слова, слова с русскими и английскими буквами,
     апострафами, двойние слова, слова с цыфрами"""
     if re.match("^[a-zA-Zа-яА-Я0-9'-_]{1,100}[ ]?[-]?[a-zA-Zа-яА-Я0-9'-_]{0,100}$", string):
-        print("\tcheck_input_str(): %s" % string)
+        log_info("\tcheck_input_str(): %s" % string)
         if title:
             return string.title()
         else:
             return string
     else:
-        logging.error("\tcheck_input_str() Fail: %s" % string)
+        log_error("\tcheck_input_str() Fail: %s" % string)
         return None
 
 
@@ -23,10 +23,10 @@ def check_input_str(string: str, title=True) -> str or None:
 def check_telegram(string: str) -> str or None:
     """ pattern: @tele, @123qwe, @qwe12, @asd11_sd1 """
     if re.match("^[@][a-zA-Zа-яА-Я_0-9]{1,100}$", string):
-        print("\tcheck_telegram(): %s" % string)
+        log_info("\tcheck_telegram(): %s" % string)
         return string
     else:
-        logging.error("\tcheck_telegram() Fail: %s" % string)
+        log_error("\tcheck_telegram() Fail: %s" % string)
         return None
 
 
@@ -34,10 +34,10 @@ def check_telegram(string: str) -> str or None:
 def check_home_number(string: str) -> str or None:
     """ pattern: 12/4, 13-4, 1a, f/2, 5/e, 6-y """
     if re.match("^[0-9a-zA-Zа-яА-Я/-]{1,10}$", string):
-        print("\tcheck_home_number(): %s" % string)
+        log_info("\tcheck_home_number(): %s" % string)
         return string
     else:
-        logging.error("\tcheck_home_number() Fail: %s" % string)
+        log_error("\tcheck_home_number() Fail: %s" % string)
         return None
 
 
@@ -45,20 +45,11 @@ def check_home_number(string: str) -> str or None:
 def check_phone(phone: str) -> str or None:
     """ pattern: +375291234567 """
     if re.match("^[+][0-9]{1,20}$", phone):
-        print("\tcheck_phone(): %s" % phone)
+        log_info("\tcheck_phone(): %s" % phone)
         return phone
     else:
-        logging.error("\tcheck_phone() Fail: %s" % phone)
+        log_error("\tcheck_phone() Fail: %s" % phone)
         return None
-
-
-# TeamRome
-def check_if_str(string: str, out: str or None) -> str:
-    print("check_if_str()")
-    if string:
-        return string
-    else:
-        return out
 
 
 # TeamRome
@@ -69,7 +60,7 @@ def try_except(foo):
         try:
             return foo(*args, **kwargs)
         except Exception as ex:
-            logging.error("\tException in - %s()\n\t%s" % (foo.__name__, ex))
+            log_error("\tException in - %s()\n\t%s" % (foo.__name__, ex))
             return None
 
     return wrapper
@@ -81,9 +72,9 @@ def time_it(foo):
 
     def wrapper(*args, **kwargs):
         time_0 = perf_counter()
-        print("%s()" % foo.__name__)
+        log_verbose("%s()" % foo.__name__)
         result = foo(*args, **kwargs)
-        print('\t%s() - OK; Time: %s sec.' % (foo.__name__, perf_counter() - time_0))
+        log_info('\t%s() - OK; TimeIt: %s sec.' % (foo.__name__, perf_counter() - time_0))
         return result
 
     return wrapper
