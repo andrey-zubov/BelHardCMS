8cc16021540f4072f9973dd5c85c9327814222ad
bequite <valentin8dev@gmail.com>
2019-10-25 18:30:37 +0300

    client profile url fix

--- client/client_urls.py
+++ client/client_urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.client_main_page, name='client'),  # main client page
 
     # Team Rome (start) - Edit Forms URLs
-    path('profile', views.ClientProfile, name='client_profile'),
+    path('profile', views.ClientProfile.as_view(), name='client_profile'),
     path('edit/', views.ClientEditMain.as_view(), name='client_edit'),
     path('edit/skills/', views.ClientEditSkills.as_view(), name='client_edit_skills'),
     path('edit/photo/', views.ClientEditPhoto.as_view(), name='client_edit_photo'),
