d84fe2676cc7a9523afb4be62d319619de11c4be
sanello <sanello@gmail.com>
2019-10-26 02:05:22 +0300

    47th commit. Creating, editing and deleting new vacancies by a recruiter are working.

--- recruit/views.py
+++ recruit/views.py
@@ -349,9 +349,6 @@ class client_task_adding(View):
         client_user = client.user_client #ссылается на UserModel
         client_activ_tasks = Tasks.objects.filter(user=client_user, status=False) #просмотр активных задач клинета
         return render(request, template_name='recruit/adding_task_to_client.html', context={'client':client,
-                                                                                            'client_user': client_user,
-                                                                                            'client_activ_tasks':client_activ_tasks,})
-
 
     def post(self, request, id_a):
         client = Client.objects.get(id=id_a)
