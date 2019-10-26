b4b4b5111de9a56b10ec3947c5a3172a56a148b3
Dima <dima.mank@gmail.com>
2019-10-26 15:34:34 +0300

    26.10

--- recruit/recruit_url.py
+++ recruit/recruit_url.py
@@ -4,10 +4,11 @@ from . import views
 
 
 
+
 urlpatterns = [
     # TeamPoland ##################################################
-    path('', views.recruiter_main_page, name='recruiter_url'),     # There is of recruiter's main page
-    path('base/', views.recruiter_base, name='base_url'),
+    # path('', views.recruiter_main_page, name='recruiter_url'),     # There is of recruiter's main page
+    #path('base/', views.recruiter_base, name='base_url'),
     path('base_of_clients/', views.base_of_applicants, name='base_of_clients'),
     path('base_of_clients/<id_a>/', views.applicant, name='applicant_url'),
     path('base_of_clients/<id_a>/tasks/', views.CreateJobInterview.as_view(), name='applicant_tasks_url'),
