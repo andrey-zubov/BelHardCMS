8152cca14cafb6c133e8c049feb7f5210ef90660
Valentin <valentin8dev@gmail.com>
2019-10-28 17:52:02 +0300

    fix (#22)

--- recruit/edit_pages/r_pages_get.py
+++ recruit/edit_pages/r_pages_get.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from BelHardCRM.settings import MEDIA_URL
 from client.edit.utility import (time_it, try_except)
 from client.models import (Sphere, Sex, Citizenship, FamilyState, Children, City, State)
-from recruit.models import (RecruitExperience, UserModel, RecruitTelephone, RecruitEducation, RecruitCertificate)
+from recruit.models import (RecruitExperience, UserModel, RecruitTelephone, RecruitEducation, RecruitCertificate,)
 from recruit.models import (RecruitSkills)
 
 
@@ -83,4 +83,48 @@ def recruit_education_page_get(recruit):  # TeamRome
                         for cert in c:
                             cert['img'] = "%s%s" % (MEDIA_URL, cert['img'])
                         e['cert'] = c
+    return response
+
+
+@try_except
+@time_it
+def recruit_show_page_get(recruit):  # TeamRome
+    response = defaultdict()
+
+    # today = date.today()
+
+    # try:
+    #   birthday = Client.date_born.replace(year=today.year)
+    # except ValueError:  # raised when birth date is February 29 and the current year is not a leap year
+    #    birthday = Client.date_born.replace(year=today.year, month=born.month + 1, day=1)
+    # if birthday > today:
+    #   age = today.year - Client.date_born.year - 1
+    # else:
+    #   pass
+
+    if recruit:
+        edus = [i for i in RecruitEducation.objects.filter(recruit_edu=recruit).values('institution', 'qualification')]
+        response['r_edu_profile'] = edus
+        exp = [i for i in
+        RecruitExperience.objects.filter(recruit_exp=recruit).values('start_date', 'end_date', 'position', 'name')]
+        response['r_exp_profile'] = exp
+        cvs = [i for i in RecruitCV.objects.filter(recruit_cv=recruit).values('position')]
+        response['r_cvs_profile'] = cvs
+        skills_arr = [i for i in RecruitSkills.objects.filter(recruit_skills=recruit).values('skill')]
+        response['r_skill_profile'] = skills_arr
+
+        user_model = UserModel.objects.get(id=recruit.user_recruiter_id)
+        response['user_model'] = {
+            "first_name": user_model.first_name,
+            "last_name": user_model.last_name,
+            "email": user_model.email,
+
+        }
+        phone_arr = [i for i in RecruitTelephone.objects.filter(recruit_phone=recruit).values("telephone_number")]
+        response['r_phone'] = phone_arr
+        # response["recruit"] = Recruit.objects.filter(user_recruit=recruit)
+        # response["recruit"] = recruit
+        # response["age"] = 9  # дописать
+        # print(response["age"])
+
     return response
