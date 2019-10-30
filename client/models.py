9e3b5b06ec0e08fc37c4634b0302f4a17bce6ee6
sanello <sanello@gmail.com>
2019-10-31 02:57:11 +0300

    49th commit is totally merged

--- client/models.py
+++ client/models.py
@@ -224,14 +224,12 @@ class CV(models.Model):
     type_salary = models.ForeignKey(TypeSalary, on_delete=models.SET_NULL,
                                     null=True, blank=True)
     # There is Poland's upgrade
-    vacancies_in_waiting = models.ManyToManyField('Vacancy', blank=True,
-                                         related_name='in_waiting_for_resume')
+    vacancies_in_waiting = models.ManyToManyField('Vacancy', blank=True, related_name='in_waiting_for_resume')
     vacancies_accept = models.ManyToManyField('Vacancy', blank=True,
                                               related_name='accept_for_resume')
     vacancies_reject = models.ManyToManyField('Vacancy', blank=True,
                                               related_name='reject_for_resume')
-    notification = models.ManyToManyField('Vacancy', blank=True,
-                                      related_name='notifications_for_resume')
+    notification = models.ManyToManyField('Vacancy', blank=True, related_name='notifications_for_resume')
 
     def __str__(self):
         return self.position
@@ -357,7 +355,7 @@ class JobInterviews(models.Model):
         for key in self.__dict__:
             if (self.__dict__[key].__class__.__name__ == 'str' or
             self.__dict__[key].__class__.__name__ == 'datetime' or
-            self.__dict__[key].__class__.__name__ == 'time' or 
+            self.__dict__[key].__class__.__name__ == 'time' or
             self.__dict__[key].__class__.__name__ == 'date'):
                 to_show_verbose_name.append(
                     self._meta.get_field(key).verbose_name)
