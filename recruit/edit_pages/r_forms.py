d5d900c2f4cb18c775d3d934dffbc2b00ed9bde9
andrey-zubov <55183393+andrey-zubov@users.noreply.github.com>
2019-11-04 18:46:00 +0300

    быстрофикс миграций. смотреть закоменченное в: -recruit/models.py -recrut/edit_pages/* -recruit/admin.py -recruit/views.py

--- recruit/edit_pages/r_forms.py
+++ recruit/edit_pages/r_forms.py
@@ -1,13 +1,13 @@
-from django import forms
-
-from recruit.models import (Recruiter)
-
-
-class RecruitUploadImgForm(forms.ModelForm):
-    class Meta:
-        model = Recruiter
-        fields = ('img',)
-
-        widgets = {
-            'img': forms.FileInput(attrs={'class': 'form-control-file'}),
-        }
+# from django import forms
+#
+# from recruit.models import (Recruiter)
+#
+#
+# class RecruitUploadImgForm(forms.ModelForm):
+#     class Meta:
+#         model = Recruiter
+#         fields = ('img',)
+#
+#         widgets = {
+#             'img': forms.FileInput(attrs={'class': 'form-control-file'}),
+#         }
