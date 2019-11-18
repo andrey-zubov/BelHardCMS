745dd1b603281fe0d12adc564e5966d7d34ce8a0
bequite <valentin8dev@gmail.com>
2019-11-18 18:43:45 +0300

    fix: Test Cases

--- client/test_edit_client/test_client_profile.py
+++ client/test_edit_client/test_client_profile.py
@@ -1,7 +1,17 @@
+import os
+import sys
+
+import django
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = os.path.expanduser(BASE_DIR)
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BelHardCRM.settings")
+django.setup()
 
 from client.models import Client
 
