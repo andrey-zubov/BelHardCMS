7e5807c3f1a35fe21d85346ae54fc185648ebd9c
andrey-zubov <55183393+andrey-zubov@users.noreply.github.com>
2019-11-29 18:44:04 +0300

    Recruter: adding task to client, change tasks, use pattern tasks/

--- recruit/views.py
+++ recruit/views.py
@@ -461,9 +461,11 @@ class client_task_adding(View):
         client = Client.objects.get(id=id_a)
         client_user = client.user_client #ссылается на UserModel
         client_activ_tasks = Tasks.objects.filter(user=client_user, status=False) #просмотр активных задач клинета
+        client_closed_tasks = Tasks.objects.filter(user=client_user, status=True)
         return render(request, template_name='recruit/adding_task_to_client.html', context={'client':client,
                                                                                             'client_user': client_user,
-                                                                                            'client_activ_tasks':client_activ_tasks,})
+                                                                                            'client_activ_tasks':client_activ_tasks,
+                                                                                            'client_closed_tasks': client_closed_tasks})
 
 
     def post(self, request, id_a):
